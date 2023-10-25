4061f6537c84fcaf42ab6bdb463e822c86589502
ibssoln <itbisys.system@gmail.com>
2023-10-25 16:43:30 -0400

    Adds clarity design framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ClarityModule } from "@clr/angular";
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
