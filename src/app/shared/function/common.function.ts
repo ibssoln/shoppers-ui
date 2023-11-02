fa688118106de73889fe69b04ae5bfabd0f095ab
ibssoln <itbisys.system@gmail.com>
2023-11-02 15:01:32 -0400

    Adds common funcions

--- src/app/shared/function/common.function.ts
+++ src/app/shared/function/common.function.ts
@@ -19,3 +19,30 @@ export function deepImmutableCopy(obj: any){
 	return JSON.parse(JSON.stringify(obj));
 }
 
+export function parseFunc(func: any){
+	if(func){
+		if(typeof func == 'string' && (func.indexOf('function')>-1 || 
+				func.indexOf('this')>-1 || func.indexOf('=>')>-1)){
+			return eval(func);
+		} else {
+			return func;
+		}
+	}
+}
+
+export function getChildFieldCol(children: any[], matchKey: string): any{
+	let found: any[] = children.columns.filter((column: any) => column.key == matchKey);
+	return (found.length>0)? found[0] : null;
+}
+
+export function getChildFieldRowColumn(children: any[], matchKey: string): any{
+	let found = {row: '', col: ''};
+	children.forEach(child => {
+		const column = getChildFieldCol(child, matchKey);
+		(column)? found = {row: child, col: column} : null;
+	});
+	return found;
+}
+
+
+
