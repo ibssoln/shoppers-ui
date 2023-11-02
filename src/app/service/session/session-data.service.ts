61a0c878aa6d5c72ee3b36e476534302c964bae4
ibssoln <itbisys.system@gmail.com>
2023-11-02 12:11:35 -0400

    Adds constants

--- src/app/service/session/session-data.service.ts
+++ src/app/service/session/session-data.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionDataService {
+  
+  private sessionData = new BehaviorSubject<SessionData>(new SessionData());
+  public sessionData$ = this.sessionData.asObserable();
+  public isRolePoliciesLoaded: boolean = false;
+
+
 
   constructor() { }
 
 
 
-  
+
 }
---
+++ src/app/shared/constant/global.const.ts
@@ -0,0 +1,26 @@
+export const API_URL = "http://localhost:8080/";
+
+export namespace Global{
+
+    export class DATAGRID{
+        public static readonly DATAGRID_DEBOUNCE: number = 500;
+    }
+    
+    export class TIMEOUT{
+        public static readonly AUTH_USERINFO_UPDATE_INTERVAL: number = 10;
+        public static readonly SESSION_IDLE_SECONDS = 600;
+        public static readonly SESSION_TIMEOUT_SECONDS = 30;
+    }
+
+    export class DATE_FORMAT{
+        public static readonly DATE_TIME_SLASH_FORMAT: string = 'MM/dd/yyyy h:mm a';
+        public static readonly DATE_TIME_DASH_FORMAT: string = 'yyyy-MM-dd HH:mm:ss';
+        public static readonly DATE_FORMAT: string = 'yyyy-MM-dd';
+        public static readonly LONG_DATE: string = 'MMMM d, y';
+    }
+
+    
+
+
+}
+
