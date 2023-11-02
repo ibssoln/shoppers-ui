0b2d034da988a1da54dc283460d1a494e5c1307c
ibssoln <itbisys.system@gmail.com>
2023-11-02 10:31:42 -0400

    Adds product service

--- src/app/service/product/product.service.ts
+++ src/app/service/product/product.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  private httpOptions = {
+            headers: new HttpHeaders({'Content-Type': 'application/json'});
+          }
+
+  public getEmailPreview(params: any): Observable<any>{
+    return this.httpClient.post<any>('http://localhost:8080/emailPreview', params, this.httpOptions).pipe(catchError(this.handleError));
+  }
+
+  public sendProofEmail(params: any): Observable<any>{
+    return this.httpClient.post<any>('http://localhost:8080/sendEmailProof', params, this.httpOptions).pipe(catchError(this.handleError));
+  }
+
+  private handleError(obj: HttpErrorResponse): any{
+    if(obj.error){
+      // this.loggerService.logError('An error occurred: ${obj.error.message}');
+    }else{
+      // this.loggerService.logError('An error occurred: status ${obj.status}, ${obj.error}');
+    }
+    return throwError(() => new Error('An error occurred. Please try again.'));
+  }  
+
 }
