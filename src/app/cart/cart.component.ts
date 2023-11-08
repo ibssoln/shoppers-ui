ac817c1e9051732b3ad2d622a25ae9a2529198d9
ibssoln <itbisys.system@gmail.com>
2023-11-07 23:51:14 -0500

    Impl multi-shop carts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -15,9 +15,11 @@ export class CartComponent {
 
   public sessionData: SessionData = new SessionData();
   public cartForm: FormGroup = this.formBuilder.group({});
+  public countShops: number = 0;
   public currentShop: Store = new Store();
   public currentCart: CartType = new CartType();
   public currentItems: CartItemType[] = [];
+  public singleShopView: boolean = false;
   public loader: boolean = false;
  
   // prevent memory leak
@@ -44,9 +46,12 @@ export class CartComponent {
 
   public loadSessionStoreInfo(){
     this.sessionData = this.sessionDataService.getSessionData();
-    this.currentShop = this.sessionData.shop;
-    this.currentCart = this.sessionData.carts.filter(cart => cart.store.id == this.currentShop.id)?.[0] ?? new CartType();
-    this.currentItems = this.currentCart?.cartItems ?? [];
+    this.countShops = this.sessionData.carts.length;
+    if (!this.singleShopView && this.countShops == 1){
+        this.currentShop = this.sessionData.shop;
+        this.currentCart = this.sessionData.carts.filter(cart => cart.store.id == this.currentShop.id)?.[0] ?? new CartType();
+        this.currentItems = this.currentCart?.cartItems ?? [];
+    }
   }
 
   public currentTotal(): number{
@@ -55,9 +60,26 @@ export class CartComponent {
     return total;
   }
 
+  public showSingleViewCart(selectedCart: CartType){
+    console.log('showSingleViewCart name = '+selectedCart.store);
+    this.currentShop = selectedCart.store;
+    this.currentCart = selectedCart;
+    this.currentItems = this.currentCart?.cartItems ?? [];
+    this.singleShopView = true;
+  }
+
+  public seeAllShopsCart(){
+    console.log('seeAllShopsCart');
+    this.currentShop = new Store();
+    this.currentCart = new CartType();
+    this.currentItems = [];
+    this.singleShopView = false;
+  }
+
   ngOnDestroy(): void{
     this.destroy$.next();
     this.destroy$.complete();
+    console.error('destroy');
   }
 
 }
