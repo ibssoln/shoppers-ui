406335c883d54d51b39098626d0a25f615b5db84
ibssoln <itbisys.system@gmail.com>
2023-11-20 23:56:28 -0500

    Adds var-write

--- src/app/var-write/var-write.component.ts
+++ src/app/var-write/var-write.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill'
 import { Subject } from 'rxjs';
@@ -23,14 +23,15 @@ export const QuillConfiguration = {
 })
 export class VarWriteComponent {
 
+  @Output() emitValue = new EventEmitter<string>();
+  
   public quillConfiguration = QuillConfiguration;
-
   public postForm: FormGroup = this.formBuilder.group({});
   public varForm: FormGroup = this.formBuilder.group({});
   public control: FormControl = new FormControl();
-  public editorInstance: any;
-
+  public editor: any;
   public loader: boolean = false;
+  public KEYWORDS = ['CUSTOMER_SERVICE','PURCHASE_SHOP'];
  
   // prevent memory leak
   private destroy$: Subject<void> = new Subject<void>();
@@ -51,250 +52,199 @@ export class VarWriteComponent {
 
   ngOnInit() {
     this.control = this.control ?? new FormControl();
-    this.postForm.addControl('varData', new FormControl(null, null));
+    // this.postForm.addControl('varData', new FormControl(null, null));
     this.varForm.addControl('varData', new FormControl(null, null));
     this.varForm.controls['varData'].setValue('');
   }
 
-  blured = false
-  focused = false
-  
-  focus($event: any) {
-    // tslint:disable-next-line:no-console
-    // console.log('focus', $event)
-    this.focused = true
-    this.blured = false
-  }
-
-  blur($event: any) {
-    // tslint:disable-next-line:no-console
-    // console.log('blur', $event)
-    this.focused = false
-    this.blured = true
+  ngAfterViewInit() {
+    this.varForm.controls['varData'].setValue('');
   }
 
   public postComment(){
-    // console.log('post');
   }
 
-  public created(editorInstance: any){
-    // tslint:disable-next-line:no-console
-    // console.log('editor-created', event)
-    this.editorInstance = editorInstance;
+  public editorChanged(event: any) {
+    this.emitValue.emit(event.editor.selection.root.innerHTML);
+  }
+
+  public editorCreated(editor: any){
+    this.editor = editor;
     this.varForm.controls['varData'].setValue('');
+    this.addBindingsOnKeyInsert();
+    this.addBindingsOnKeyDelete();
+  }
 
-    //------------ additional -------
-    this.editorInstance.keyboard.addBinding({ key: 'Delete' }, 
-    {
-      format: ['code']
-    },
-    (range: any, context: any) => {
-      console.log('# Delete = range='+JSON.stringify(range)+', conext='+JSON.stringify(context));
-    });
+  public getHtml(){
+    const html = this.editor.clipboard.convert(
+      this.editor.selection.root.innerHTML
+    );
+    return html;
+  }
 
-    //------------ additional -------
-    this.editorInstance.keyboard.addBinding({ key: 'K', shiftKey: null, altKey: null, metaKey:null, ctrlKey:null, shortKey: null }, 
-    {
-      format: ['code']
-    },
-    (range: any, context: any) => {
-      console.log('# K = range='+JSON.stringify(range)+', conext='+JSON.stringify(context));
-    });
-
-    //------------ final solution -------
-    this.editorInstance.keyboard.addBinding({ key: 'Backspace' }, 
-    {
-      format: ['code', 'list']
-    },
-    (range: any, context: any) => {
-      console.error('Backspace pressed');
-      if(context.format.list){
-        console.error('list');
-        this.editorInstance.format('list', false);
-      }else{
-        console.error('code');
-        //------------ determine which element was clicked & delte it --------
-        const index = this.editorInstance.getSelection(true).index;
-        const clip = this.editorInstance.clipboard.convert(
-          this.editorInstance.selection.root.innerHTML
-        );    
-        console.log('html = '+JSON.stringify(clip));
-        let count = 0;
-        let before, after, iter = 0;
-        let found: any;
-        let newText = new Delta();
-        clip.map((op: any) => {
-          iter++;
-          before = count;
-          count += op.insert.length;
-          after = count;
-          if((index > before) && (index <= after)){
-            found = op.insert+iter;
-            // newText.insert(op.insert, op.attributes);
-          }else{
-            newText.insert(op.insert, op.attributes);
-          }
-        })
-        .join('');
-        console.log('total count = '+count);
-        console.log('before = '+before);
-        console.log('====> index = '+index+'<======');
-        console.log('after = '+after);
-        console.log('>> found = '+found);
-        console.log('total iter = '+iter);
-        //------------ determine which element was clicked --------
-        //------------------ change the results ===============
-        // let count2 = 0;
-        // let final, skip: any;
-        // let newText = new Delta();
-        // const text = clip
-        // // .filter((op: any) => typeof op.insert == 'string')
-        // .map((op: any) => {
-        //   count2++;
-        //   if(op.insert+count2 != found){
-        //     final = op.insert+count2;
-        //     newText.insert(op.insert, op.attributes);
-        //   }else{
-        //     skip = op.insert+count2;
-        //     // newText.delete(op.insert);
-        //   }
-        // })
-        // .join('');
-        // console.log('#final  = '+final);
-        // console.log('#skip  = '+skip);
-        console.log('newText = '+JSON.stringify(newText));
-        //parse
-        // const clipboardDelta = this.editorInstance.clipboard.convert(
-        //   newText
-        // );
-
-        //---------------- empty out
-        this.emptyOut();
-        //---------------- empty out
-       
-        const delta = new Delta().concat(newText);
-        this.editorInstance.updateContents(delta, 'user');
-        //------------------ change the results ===============
+  public getIndexInfo(index: any, html: any): any{
+    let i=0, prev=0, post=0, pointer=0, isCodeBlock=false, elemSeq=0, elems: Array<any>=[];
+    let pos: {prev: any, post: any} = {prev: 0, post: 0};
+    html.map((op: any)=> {
+      elems.push(op);
+      prev = pointer;
+      pointer += op.insert.length;
+      post = pointer;
+      if(((index==0 && prev==0) || (index > prev)) && (index <= post)){
+        elemSeq = i;
+        isCodeBlock = !!(op.attributes?.code);
+        pos = {prev, post};
       }
+      i++;
     });
+    return {elemSeq, isCodeBlock, pos, elems};
   }
 
-  changedEditor(event: EditorChangeContent | EditorChangeSelection) {
-    // tslint:disable-next-line:no-console
-    // console.log('editor-change', event)
+  public enterVariable(fieldName: any): void{
+    const varValue = this.varForm.controls[fieldName].value;
+    if(varValue){
+      const html = this.getHtml();
+      const index = this.editor.getSelection(true).index;
+      let {elemSeq, isCodeBlock, pos, elems} = this.getIndexInfo(index, html);
+      let cursorPosition = index;
+      if(isCodeBlock){
+        cursorPosition = pos.post+1;
+      }
+      cursorPosition = this.insertValue(cursorPosition, varValue, `<code>${varValue}</code>`);
+      cursorPosition = this.insertValue(cursorPosition, ' ', `<p>&nbsp;</p>`);
+      this.assignAndEmitNewValue(cursorPosition);
+    }
   }
 
-  // type1
-  //public enterVariable(event: any){
-    // console.log('enterVariable = '+event);
-    // const selectedIndex = this.editorInstance.getSelection(true).index;
-    // console.log('selectedIndex = '+selectedIndex);
-    // const selected = this.varForm.controls['varData'].value+' ';
-    // this.editorInstance.insertText(selectedIndex, selected);
-    // this.editorInstance.setSelection(selectedIndex + selected.length);
-    // this.varForm.controls['varData'].setValue('');
-  //}
-
-  // type2
-  public enterVariable(event: any): void{
-    console.log('testEnter1');
-    const cursorPosition = this.editorInstance.getSelection(true).index;
-    const selected = this.varForm.controls['varData'].value;
-    const clipboardDelta = this.editorInstance.clipboard.convert(
-      `<code>${selected}</code>`
-    );
-    const delta = new Delta().retain(cursorPosition).concat(clipboardDelta);
-    this.editorInstance.updateContents(delta, 'user');
-    this.editorInstance.setSelection(Number(cursorPosition + selected.length));
-
-    const cursorPosition2 = this.editorInstance.getSelection(true).index;
-    const space = this.editorInstance.clipboard.convert(
-      `<p>&nbsp;</p>`
-    );
-    const delta2 = new Delta().retain(cursorPosition2).concat(space);
-    this.editorInstance.updateContents(delta2, 'user');
-
-    const controlNewValue = this.editorInstance.selection.root.innerHTML;
-    console.log('emit = '+controlNewValue);
-    this.editorInstance.setSelection(Number(cursorPosition2 + 1));
+  private assignAndEmitNewValue(cursorPosition: any): void{
+    const html = this.editor.selection.root.innerHTML;
+    this.control.setValue(html);
+    this.emitValue.emit(html);
+    this.editor.setSelection(cursorPosition);
   }
 
-  //type3
-  public onKeyDown(event: any){
-    const index = this.editorInstance.getSelection(true).index;
-
-    const clip = this.editorInstance.clipboard.convert(
-      this.editorInstance.selection.root.innerHTML
-    );    
-    console.log('html = '+JSON.stringify(clip));
-
-    //------------ determine which element was clicked --------
-    // let count = 0;
-    // let before, after, iter = 0;
-    // let found: any;
-    // clip
-    // .map((op: any) => {
-    //   iter++;
-    //   before = count;
-    //   count += op.insert.length;
-    //   after = count;
-    //   if((count > before) && (count <= after)){
-    //     found = op.insert+iter
-    //   }
-    // })
-    // .join('');
-    // console.log('total count = '+count);
-    // console.log('before = '+before);
-    // console.log('count = '+count);
-    // console.log('after = '+after);
-    // console.log('found = '+found);
-    //------------ determine which element was clicked --------
-
-    //------------------ change the results ===============
-    // let newText = new Delta();
-    // const text = clip
-    // .filter((op: any) => typeof op.insert == 'string')
-    // .map((op: any) => {
-    //   if(op.insert=='[CUSTOMER_SERVICE]'){
-    //     newText.insert(op.insert, op.attributes);
-    //   }else{
-    //     // newText.delete(op.insert);
-    //   }
-    // })
-    // .join('');
-    // console.log('text = '+JSON.stringify(newText));
-    // //parse
-    // // const clipboardDelta = this.editorInstance.clipboard.convert(
-    // //   newText
-    // // );
-    // const delta = new Delta().concat(newText);
-    // this.editorInstance.updateContents(delta, 'user');
-    //------------------ change the results ===============
-
-    // console.log('onKeyDown');
-    // if(event.key=='Backspace'){
-    //   // console.log('Backspace');
-
-    // }else if(event.key=='Delete'){
-    //   // console.log('Delete');
-    
-    // }
+  public insertValue(position: any, textOnly: any, textWithTag: any): any{
+    const clip = this.editor.clipboard.convert(textWithTag);
+    const delta = new Delta().retain(position).concat(clip);
+    this.editor.updateContents(delta, 'user');
+    const newCursorPos = Number(position + textOnly.length);
+    this.editor.setSelection(newCursorPos);
+    return newCursorPos;
   }
 
-  //type4
+  public preventInsert(range: any, context: any){};
+
+  public assessTabEnterUpdateRequired(html: any, elemSeq: any, criteria: any, elems: any): any{
+    let updateReq = {req: false, index: 0};
+    let i=0;
+    html.map((op: any) => {
+      if((i>=(elemSeq-1)) && (i<=(elemSeq+1))){
+        if(op.insert==criteria){
+          if((i>0) && (i<(elems.length-1))
+          && !!(elems[i-1].attributes?.code) && !!(elems[i+1].attributes?.code)
+          && !!(elems[i-1].insert) && !!(elems[i+1].insert)
+          && this.includeReservedKeyword(elems[i-1].insert.concat(elems[i+1].insert))
+          ){
+            updateReq = {req: true, index: i};
+          }
+        }
+      }
+      i++;
+    });
+    return updateReq;
+  }
+
+  public processTabEnterUpdateText(elems: any, updateReq: any): any{
+    let k=0;
+    let updateText = new Delta();
+    elems.map((op: any) => {
+      if((k==(updateReq.index-1)) || (k==(updateReq.index+1))){
+        //skip
+      }else if(k==updateReq.index){
+        updateText.insert((elems[k-1].insert.replace(/\s/g, "").toUpperCase().concat(elems[k+1].insert.replace(/\s/g, '').toUpperCase())), elems[k-1].attributes);
+      }else{
+        updateText.insert(op.insert, op.attributes);
+      }
+      k++;
+    });
+    return updateText;
+  }
+
+  public onKeydown(event: any){
+    if(event.key=='Tab' || event.key=='Enter'){
+      let criteria = '';
+      if(event.key=='Enter'){
+        criteria='\n';
+      }
+      if(event.key=='Tab'){
+        criteria='\t';
+      }
+      const html = this.getHtml();
+      const index = this.editor.getSelection(true).index;
+      let {elemSeq, isCodeBlock, pos, elems} = this.getIndexInfo(index, html);
+      let updateReq = this.assessTabEnterUpdateRequired(html, elemSeq, criteria, elems);
+      if(!!updateReq.req){
+        let updateText = this.processTabEnterUpdateText(elems, updateReq);
+        this.emptyOut();
+        const delta = new Delta().concat(updateText);
+        this.editor.updateContents(delta, 'user');
+        this.editor.setSelection(index);
+      }
+    }
+  }
+
+  public isReservedKeyword(checkWord: string): boolean{
+    return ((this.KEYWORDS.filter((word: string)=>(
+      word.localeCompare(checkWord.replace(/\s/g, '').toUpperCase())==0))).length > 0);
+  }
+
+  public includeReservedKeyword(checkWord: string): boolean{
+    return ((this.KEYWORDS.filter((word: string)=>(
+      checkWord.replace(/\s/g, '').toUpperCase().includes(word)))).length > 0);
+  }
+
+  public handleDeletionOnCode(range: any, context: any): void{
+    if(this.includeReservedKeyword(context.prefix.concat(context.suffix))){
+      const html = this.getHtml();
+      const index = this.editor.getSelection(true).index;
+      let {pos} = this.getIndexInfo(index, html); 
+      this.editor.deleteText(pos.prev, Number(pos.post-pos.prev));
+      this.editor.setSelection(pos.prev);
+    }
+  }
+
+  private getAllIndexOf(target: string, match: string, count: number): any{
+    let indices = [];
+    let checkStart = 0, iter: number = 0;
+    while(iter < count){
+      let index = target.indexOf(match, checkStart);
+      indices.push(index);
+      checkStart = index+1;
+      iter++;
+    }
+    return indices;
+  }
+
   public emptyOut(){
+    const existing = this.editor.selection.root.innerHTML;
+    const clipboardDelta = this.editor.clipboard.convert(
+      `<p></p>`
+    );
+    const delta = new Delta().delete(existing.length).concat(clipboardDelta);
+    this.editor.updateContents(delta, 'user');
+    const afterChanged = this.editor.selection.root.innerHTML;
+  }
 
-    // if(event.key=='Delete'){
-      console.log('Delete');
-      const existing = this.editorInstance.selection.root.innerHTML;
-      const clipboardDelta = this.editorInstance.clipboard.convert(
-        `<p><p>`
-      );
-      const delta = new Delta().delete(existing.length).concat(clipboardDelta);
-      this.editorInstance.updateContents(delta, 'user');
-      const afterChanged = this.editorInstance.selection.root.innerHTML;
-      console.log('after changed = '+JSON.stringify(afterChanged));
-    // }
+  public addBindingsOnKeyDelete(){
+    this.editor.keyboard.addBinding({key: 'Backspace'}, {format: ['code']},
+    (range: any, context: any)=>{this.handleDeletionOnCode(range, context)});
+    this.editor.keyboard.addBinding({key: 'Delete'}, {format: ['code']},
+    (range: any, context: any)=>{this.handleDeletionOnCode(range, context)});
+  }
+
+  public addBindingsOnKeyInsert(){
+    this.editor.keyboard.addBinding({key: 'A', metaKey: null, ctrlKey: null, shiftKey: null, altKey: null, shortKey: null}, {format: ['code']}, this.preventInsert);
+  
   }
 
   ngOnDestroy(): void{
