b56611b23c525a187a010a1b889ab1c4828727a7
ibssoln <itbisys.system@gmail.com>
2023-12-14 20:58:02 -0500

    Adds build scripts

---
+++ build/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -v
+
+## angular cli build
+npm run ng build -- --configuration production
+
+## create and populate a folder
+if [ -d "target" ]
+then 
+  rm -r target
+  mkdir target
+else
+  mkdir target
+fi
+
+## copy node and angular code
+cp -r dist target/dist
+cp -r bulid target/build
+cp server.js target
+cp default.env target
+cp -r server target
+cp .npmrc target
+cp package.json target
+cp Dockerfile target
+
+## download dependencies
+cd target
+node build/buildstamp.js $1
+
+cd ..
